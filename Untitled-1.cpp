429a2ccf230bf5fa6d95ec04c3e5c8336cf59c06
MarBamboo <MarBamboo@163.com>
2020-11-22 14:38:03 +0300

    first

---
+++ Untitled-1.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include "stdio.h"
+#include "stdint.h"
+
+using namespace std;
+
+
+int main(void)
+{
+    while (1)
+    {
+        cout<<"hello world "<<endl;
+    }
+    
+
+    
+}
